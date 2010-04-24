7a777cb795168b0f85735bd9ab61ac5d69d9d4b9
buri <buri@1f99f4d6-3056-11df-954b-21e382534593>
2010-04-24 19:38:58 +0000

    Tagging v1.2.1.

---
+++ tags/ddmsence-1.2.1/src/main/buri/ddmsence/util/PropertyReader.java
@@ -0,0 +1,188 @@
+/* Copyright 2010 by Brian Uri!
+   
+   This file is part of DDMSence.
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 3.0 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public 
+   License along with DDMSence. If not, see <http://www.gnu.org/licenses/>.
+
+   You can contact the author at ddmsence@urizone.net. The DDMSence
+   home page is located at http://ddmsence.urizone.net/
+*/
+package buri.ddmsence.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Utility class for dealing with the property file.
+ * 
+ * <p>
+ * Properties in DDMSence are found in the <code>ddmsence.properties</code> file. All properties are prefixed with
+ * "buri.ddmsence.", so <code>getProperty</code> calls should be performed with just the property suffix.
+ * </p>
+ * 
+ * <p>
+ * The Property Reader supports several custom properties, which can be specified at runtime in a 
+ * <code>ddmsence-config.properties</code> file. If the file exists in the classpath, the values from that file will
+ * be used. Only a subset of the DDMSence properties are allowed to be configured:
+ * </p>
+ * 
+ * <ul>
+ * <li><code>buri.ddmsence.ddms.prefix</code>: The namespace prefix for components in the DDMS namespace.</li>
+ * <li><code>buri.ddmsence.gml.prefix</code>: The namespace prefix for components in the GML namespace.</li>
+ * <li><code>buri.ddmsence.icism.prefix</code>: The namespace prefix for components in the ICISM namespace.</li>
+ * <li><code>buri.ddmsence.xlink.prefix</code>: The namespace prefix for components in the xlink namespace.</li>
+ * <li><code>buri.ddmsence.sample.data</code>: The default location for Sample Application data.</li>
+ * </ul> 
+ * 
+ * <p>
+ * Changing a namespace prefix will affect both components created from scratch and components loaded from XML files.
+ * </p>
+ * 
+ * @author Brian Uri!
+ * @since 0.9.b
+ */
+public class PropertyReader {
+	private Properties _properties = new Properties();
+	
+	private static final String PROPERTIES_FILE = "ddmsence.properties";
+	private static final String CUSTOM_PROPERTIES_FILE = "ddmsence-config.properties";
+	private static final String PROPERTIES_PREFIX = "buri.ddmsence.";
+	private static final String UNDEFINED_PROPERTY = "Undefined Property: ";
+	private static final String NOT_NUMBER_PROPERTY = "Not a Number: ";
+	
+	private static final Set<String> CUSTOM_PROPERTIES = new HashSet<String>();
+	static {
+		CUSTOM_PROPERTIES.add(PROPERTIES_PREFIX + "ddms.prefix");
+		CUSTOM_PROPERTIES.add(PROPERTIES_PREFIX + "gml.prefix");
+		CUSTOM_PROPERTIES.add(PROPERTIES_PREFIX + "icism.prefix");
+		CUSTOM_PROPERTIES.add(PROPERTIES_PREFIX + "xlink.prefix");
+		CUSTOM_PROPERTIES.add(PROPERTIES_PREFIX + "sample.data");
+	};
+	
+	private static final PropertyReader INSTANCE = new PropertyReader();
+    
+	/**
+	 * Private to prevent instantiation
+	 */
+	private PropertyReader() {
+		InputStream is = getLoader().getResourceAsStream(PROPERTIES_FILE);
+		try {
+			if (is != null) {
+				Properties aProperties = new Properties();
+				aProperties.load(is);
+				is.close();
+				_properties.putAll(aProperties);
+				loadCustomProperties();
+			}
+		}
+		catch (IOException e) {
+			throw new RuntimeException("Could not load the properties file: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Searches for the ddmsence-config.properties file and loads specifically
+	 * defined custom properties, if available.
+	 * 
+	 * @throws IOException
+	 */
+	private void loadCustomProperties() throws IOException {
+		InputStream is = getLoader().getResourceAsStream(CUSTOM_PROPERTIES_FILE);
+		if (is == null)
+			return;
+		Properties aProperties = new Properties();
+		aProperties.load(is);
+		is.close();
+		for (String customKey : CUSTOM_PROPERTIES) {
+			String customProperty = aProperties.getProperty(customKey);
+			if (customProperty != null) {
+				_properties.put(customKey, customProperty);
+			}
+		}
+	}
+		
+	/**
+	 * Locates a property and returns it. Assumes that the property is required.
+	 * 
+	 * @param name		the simple name of the property, without "buri.ddmsence."
+	 * @return the property specified
+	 * @throws IllegalArgumentException if the property does not exist.
+	 */
+	public static String getProperty(String name) {
+		String value = INSTANCE.getProperties().getProperty(PROPERTIES_PREFIX + name);
+		if (value == null)
+			throw new IllegalArgumentException(UNDEFINED_PROPERTY + PROPERTIES_PREFIX + name);
+		return (value);
+	}
+
+	/**
+	 * Locates a property and returns it as an int
+	 * 
+	 * @param name		the simple name of the property, without "buri.ddmsence."
+	 * @return the property specified
+	 * @throws IllegalArgumentException if the property does not exist or is not a number.
+	 */
+	public static int getIntProperty(String name) {
+		String value = getProperty(name);
+		try {
+			int intValue = Integer.valueOf(value);
+			return (intValue);
+		}
+		catch (NumberFormatException e) {
+			throw new IllegalArgumentException(NOT_NUMBER_PROPERTY + value);
+		}
+	}
+	
+	/**
+	 * Locates a list property and returns it as a List
+	 * 
+	 * @param name		the simple name of the property, without "buri.ddmsence."
+	 * @return the property specified
+	 * @throws IllegalArgumentException if the property does not exist
+	 */
+	public static List<String> getListProperty(String name) {
+		String value = getProperty(name);
+		String[] values = value.split(",");
+		List<String> listValues = Arrays.asList(values);
+		return (Collections.unmodifiableList(listValues));
+	}	
+	
+	/**
+	 * Generate a ClassLoader to be used to load resources
+	 * 
+	 * @return a ClassLoader
+	 */
+	private static ClassLoader getLoader() {
+		return new FindClassLoader().getClass().getClassLoader();
+	}
+
+	/**
+	 * Stub to load classes.
+	 */
+	private static class FindClassLoader {
+		public FindClassLoader() {}
+	} 
+	
+	/**
+	 * Accessor for properties object.
+	 */
+	private Properties getProperties() {
+		return (_properties);
+	}
+}
---
+++ tags/ddmsence-1.2.1/src/test/buri/ddmsence/ddms/resource/SubtitleTest.java
@@ -0,0 +1,259 @@
+/* Copyright 2010 by Brian Uri!
+   
+   This file is part of DDMSence.
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 3.0 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public 
+   License along with DDMSence. If not, see <http://www.gnu.org/licenses/>.
+
+   You can contact the author at ddmsence@urizone.net. The DDMSence
+   home page is located at http://ddmsence.urizone.net/
+ */
+package buri.ddmsence.ddms.resource;
+
+import nu.xom.Element;
+import buri.ddmsence.ddms.AbstractComponentTestCase;
+import buri.ddmsence.ddms.InvalidDDMSException;
+import buri.ddmsence.ddms.ValidationMessage;
+import buri.ddmsence.ddms.security.SecurityAttributes;
+import buri.ddmsence.ddms.security.SecurityAttributesTest;
+import buri.ddmsence.util.DDMSVersion;
+import buri.ddmsence.util.Util;
+
+/**
+ * <p>Tests related to ddms:subtitle elements</p>
+ * 
+ * @author Brian Uri!
+ * @since 0.9.b
+ */
+public class SubtitleTest extends AbstractComponentTestCase {
+
+	private static final String TEST_VALUE = "Version 0.1";
+
+	/**
+	 * Constructor
+	 */
+	public SubtitleTest() {
+		super("subtitle.xml");
+	}
+
+	/**
+	 * Attempts to build a component from a XOM element.
+	 * 
+	 * @param expectFailure true if this operation is expected to fail, false otherwise
+	 * @param element the element to build from
+	 * 
+	 * @return a valid object
+	 */
+	private Subtitle testConstructor(boolean expectFailure, Element element) {
+		Subtitle component = null;
+		try {
+			component = new Subtitle(element);
+			checkConstructorSuccess(expectFailure);
+		} catch (InvalidDDMSException e) {
+			checkConstructorFailure(expectFailure, e);
+		}
+		return (component);
+	}
+
+	/**
+	 * Helper method to create an object which is expected to be valid.
+	 * 
+	 * @param expectFailure true if this operation is expected to succeed, false otherwise
+	 * @param description the description child text
+	 * @return a valid object
+	 */
+	private Subtitle testConstructor(boolean expectFailure, String description) {
+		Subtitle component = null;
+		try {
+			component = new Subtitle(description, SecurityAttributesTest.getFixture(false));
+			checkConstructorSuccess(expectFailure);
+		} catch (InvalidDDMSException e) {
+			checkConstructorFailure(expectFailure, e);
+		}
+		return (component);
+	}
+
+	/**
+	 * Returns the expected HTML output for this unit test
+	 */
+	private String getExpectedHTMLOutput() {
+		StringBuffer html = new StringBuffer();
+		html.append("<meta name=\"subtitle\" content=\"").append(TEST_VALUE).append("\" />\n");
+		html.append("<meta name=\"subtitle.classification\" content=\"U\" />\n");
+		html.append("<meta name=\"subtitle.ownerProducer\" content=\"USA\" />\n");
+		return (html.toString());
+	}
+
+	/**
+	 * Returns the expected Text output for this unit test
+	 */
+	private String getExpectedTextOutput() {
+		StringBuffer text = new StringBuffer();
+		text.append("Subtitle: ").append(TEST_VALUE).append("\n");
+		text.append("Subtitle Classification: U\n");
+		text.append("Subtitle ownerProducer: USA\n");
+		return (text.toString());
+	}
+
+	/**
+	 * Returns the expected XML output for this unit test
+	 */
+	private String getExpectedXMLOutput() {
+		StringBuffer xml = new StringBuffer();
+		xml.append("<ddms:subtitle xmlns:ddms=\"").append(DDMSVersion.getCurrentVersion().getNamespace()).append("\" ");
+		xml.append("xmlns:ICISM=\"").append(DDMSVersion.getCurrentVersion().getIcismNamespace()).append("\" ");
+		xml.append("ICISM:classification=\"U\" ICISM:ownerProducer=\"USA\">");
+		xml.append(TEST_VALUE).append("</ddms:subtitle>");
+		return (xml.toString());
+	}
+
+	public void testNameAndNamespace() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			Subtitle component = testConstructor(WILL_SUCCEED, getValidElement(version));
+			assertEquals(Subtitle.NAME, component.getName());
+			assertEquals(Util.DDMS_PREFIX, component.getPrefix());
+			assertEquals(Util.DDMS_PREFIX + ":" + Subtitle.NAME, component.getQualifiedName());
+
+			// Wrong name/namespace
+			Element element = Util.buildDDMSElement("wrongName", null);
+			testConstructor(WILL_FAIL, element);
+		}
+	}
+
+	public void testElementConstructorValid() throws InvalidDDMSException {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			// All fields
+			testConstructor(WILL_SUCCEED, getValidElement(version));
+
+			// No optional fields
+			Element element = Util.buildDDMSElement(Subtitle.NAME, null);
+			SecurityAttributesTest.getFixture(false).addTo(element);
+			testConstructor(WILL_SUCCEED, element);
+		}
+	}
+
+	public void testDataConstructorValid() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			// All fields
+			testConstructor(WILL_SUCCEED, TEST_VALUE);
+
+			// No optional fields
+			testConstructor(WILL_SUCCEED, "");
+		}
+	}
+
+	public void testElementConstructorInvalid() throws InvalidDDMSException {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			// Wrong name
+			Element element = Util.buildDDMSElement("unknownName", null);
+			SecurityAttributesTest.getFixture(false).addTo(element);
+			testConstructor(WILL_FAIL, element);
+		}
+	}
+
+	public void testDataConstructorInvalid() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			// Bad security attributes
+			try {
+				new Subtitle(TEST_VALUE, (SecurityAttributes) null);
+				fail("Allowed invalid data.");
+			} catch (InvalidDDMSException e) {
+				// Good
+			}
+		}
+	}
+
+	public void testWarnings() throws InvalidDDMSException {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			// No warnings
+			Subtitle component = testConstructor(WILL_SUCCEED, getValidElement(version));
+			assertEquals(0, component.getValidationWarnings().size());
+
+			// No value
+			Element element = Util.buildDDMSElement(Subtitle.NAME, null);
+			SecurityAttributesTest.getFixture(false).addTo(element);
+			component = testConstructor(WILL_SUCCEED, element);
+			assertEquals(1, component.getValidationWarnings().size());
+			assertEquals(ValidationMessage.WARNING_TYPE, component.getValidationWarnings().get(0).getType());
+			assertEquals("A ddms:subtitle element was found with no subtitle value.", 
+				component.getValidationWarnings().get(0).getText());
+			assertEquals("/ddms:subtitle", component.getValidationWarnings().get(0).getLocator());
+		}
+	}
+
+	public void testConstructorEquality() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			Subtitle elementComponent = testConstructor(WILL_SUCCEED, getValidElement(version));
+			Subtitle dataComponent = testConstructor(WILL_SUCCEED, TEST_VALUE);
+			assertEquals(elementComponent, dataComponent);
+			assertEquals(elementComponent.hashCode(), dataComponent.hashCode());
+		}
+	}
+
+	public void testConstructorInequalityDifferentValues() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			Subtitle elementComponent = testConstructor(WILL_SUCCEED, getValidElement(version));
+			Subtitle dataComponent = testConstructor(WILL_SUCCEED, DIFFERENT_VALUE);
+			assertFalse(elementComponent.equals(dataComponent));
+		}
+	}
+
+	public void testConstructorInequalityWrongClass() throws InvalidDDMSException {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			Subtitle elementComponent = testConstructor(WILL_SUCCEED, getValidElement(version));
+			Rights wrongComponent = new Rights(true, true, true);
+			assertFalse(elementComponent.equals(wrongComponent));
+		}
+	}
+
+	public void testHTMLOutput() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			Subtitle component = testConstructor(WILL_SUCCEED, getValidElement(version));
+			assertEquals(getExpectedHTMLOutput(), component.toHTML());
+
+			component = testConstructor(WILL_SUCCEED, TEST_VALUE);
+			assertEquals(getExpectedHTMLOutput(), component.toHTML());
+		}
+	}
+
+	public void testTextOutput() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			Subtitle component = testConstructor(WILL_SUCCEED, getValidElement(version));
+			assertEquals(getExpectedTextOutput(), component.toText());
+
+			component = testConstructor(WILL_SUCCEED, TEST_VALUE);
+			assertEquals(getExpectedTextOutput(), component.toText());
+		}
+	}
+
+	public void testXMLOutput() {
+		for (String version : DDMSVersion.getSupportedVersions()) {
+			DDMSVersion.setCurrentVersion(version);
+			Subtitle component = testConstructor(WILL_SUCCEED, getValidElement(version));
+			assertEquals(getExpectedXMLOutput(), component.toXML());
+
+			component = testConstructor(WILL_SUCCEED, TEST_VALUE);
+			assertEquals(getExpectedXMLOutput(), component.toXML());
+		}
+	}
+}
