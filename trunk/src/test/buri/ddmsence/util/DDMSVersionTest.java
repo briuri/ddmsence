14500f4958bd742e4a55c37e0467ca2a27390564
buri <buri@1f99f4d6-3056-11df-954b-21e382534593>
2011-08-28 18:08:01 +0000

    Issue #147: Expand schema hierarchy for 2.0 - 3.1.

--- trunk/src/test/buri/ddmsence/util/DDMSVersionTest.java
+++ trunk/src/test/buri/ddmsence/util/DDMSVersionTest.java
@@ -85,7 +85,7 @@ public class DDMSVersionTest extends TestCase {
 	}
 	
 	public void testGetCurrentSchema() {
-		assertEquals("/schemas/3.1/DDMS-v3_1.xsd", DDMSVersion.getCurrentVersion().getSchema());
+		assertEquals("/schemas/3.1/DDMS/DDMS-v3_1.xsd", DDMSVersion.getCurrentVersion().getSchema());
 	}
 	
 	public void testGetCurrentNamespace() {
@@ -109,7 +109,7 @@ public class DDMSVersionTest extends TestCase {
 	
 	public void testGetSchemaForValid() {
 		DDMSVersion.setCurrentVersion("2.0");
-		assertEquals("/schemas/2.0/DDMS-v2_0.xsd", DDMSVersion.getCurrentVersion().getSchema());
+		assertEquals("/schemas/2.0/DDMS/DDMS-v2_0.xsd", DDMSVersion.getCurrentVersion().getSchema());
 	}
 	
 	public void testIsCurrentVersion() {
@@ -126,9 +126,9 @@ public class DDMSVersionTest extends TestCase {
 		DDMSVersion version = DDMSVersion.getCurrentVersion();
 		assertEquals("3.0", version.getVersion());
 		assertEquals("http://metadata.dod.mil/mdr/ns/DDMS/3.0/", version.getNamespace());
-		assertEquals("/schemas/3.0/DDMS-v3_0.xsd", version.getSchema());
+		assertEquals("/schemas/3.0/DDMS/DDMS-v3_0.xsd", version.getSchema());
 		assertEquals("http://www.opengis.net/gml/3.2", version.getGmlNamespace());
-		assertEquals("/schemas/3.0/DDMS-GML-Profile.xsd", version.getGmlSchema());
+		assertEquals("/schemas/3.0/DDMS/DDMS-GML-Profile.xsd", version.getGmlSchema());
 		assertEquals("urn:us:gov:ic:ism", version.getIcismNamespace());
 	}
 	
