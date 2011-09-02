6035d2ba8b68bc87f1a523a63eb45c17df75b670
buri <buri@1f99f4d6-3056-11df-954b-21e382534593>
2011-09-02 19:17:52 +0000

    Restored smaller constructors to prevent another backwards compatibility break.

--- src/main/buri/ddmsence/ddms/summary/Category.java
+++ src/main/buri/ddmsence/ddms/summary/Category.java
@@ -93,6 +93,18 @@ public final class Category extends AbstractBaseComponent {
 	}
 	
 	/**
+	 * Constructor for creating a component from raw data. Provided for backwards compatibility to pre-DDMS 4.0 elements.
+	 *  
+	 * @param qualifier the qualifier (optional)
+	 * @param code the code (optional)
+	 * @param label the label (required)
+	 * @throws InvalidDDMSException if any required information is missing or malformed
+	 */
+	public Category(String qualifier, String code, String label) throws InvalidDDMSException {
+		this(qualifier, code, label, null, null);
+	}
+	
+	/**
 	 * Constructor for creating a component from raw data
 	 *  
 	 * @param qualifier the qualifier (optional)
