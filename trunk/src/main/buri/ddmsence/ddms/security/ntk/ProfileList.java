40803a3abdd0989f5ec8df76adc8cb7f6658c169
buri <buri@1f99f4d6-3056-11df-954b-21e382534593>
2011-09-16 12:21:37 +0000

    Issue #109: New component, ProfileList.

---
+++ trunk/src/main/buri/ddmsence/ddms/security/ntk/ProfileList.java
@@ -0,0 +1,277 @@
+/* Copyright 2010 - 2011 by Brian Uri!
+   
+   This file is part of DDMSence.
+   
+   This library is free software; you can redistribute it and/or modify
+   it under the terms of version 3.0 of the GNU Lesser General Public 
+   License as published by the Free Software Foundation.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public 
+   License along with DDMSence. If not, see <http://www.gnu.org/licenses/>.
+
+   You can contact the author at ddmsence@urizone.net. The DDMSence
+   home page is located at http://ddmsence.urizone.net/
+*/
+package buri.ddmsence.ddms.security.ntk;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import nu.xom.Element;
+import nu.xom.Elements;
+import buri.ddmsence.AbstractBaseComponent;
+import buri.ddmsence.ddms.IBuilder;
+import buri.ddmsence.ddms.IDDMSComponent;
+import buri.ddmsence.ddms.InvalidDDMSException;
+import buri.ddmsence.ddms.security.ism.SecurityAttributes;
+import buri.ddmsence.util.DDMSVersion;
+import buri.ddmsence.util.LazyList;
+import buri.ddmsence.util.PropertyReader;
+import buri.ddmsence.util.Util;
+
+/**
+ * An immutable implementation of ntk:AccessProfileList.
+ * 
+ * <p>Unlike ntk:AccessIndividualList and ntk:AccessGroupList, this element is implemented in DDMSence because it has security attributes.
+ * 
+ * <table class="info"><tr class="infoHeader"><th>Nested Elements</th></tr><tr><td class="infoBody">
+ * <u>ntk:AccessProfile</u>: A system access record matching a profile (1-to-many required), implemented as an {@link Profile}<br />
+ * </td></tr></table>
+ * 
+ * <table class="info"><tr class="infoHeader"><th>Attributes</th></tr><tr><td class="infoBody">
+ * This class is decorated with ISM {@link SecurityAttributes}. The classification and
+ * ownerProducer attributes are required.
+ * </td></tr></table>
+ *  
+ * <table class="info"><tr class="infoHeader"><th>DDMS Information</th></tr><tr><td class="infoBody">
+ * <u>Description</u>: An access record describing system access for profiles.<br />
+ * <u>Obligation</u>: Optional<br />
+ * <u>Schema Modification Date</u>: 2011-08-31<br />
+ * </td></tr></table>
+ * 
+ * @author Brian Uri!
+ * @since 2.0.0
+ */
+public final class ProfileList extends AbstractBaseComponent {
+	
+	// Values are cached upon instantiation, so XOM elements do not have to be traversed when calling getters.
+	private List<Profile> _cachedProfiles;
+	private SecurityAttributes _cachedSecurityAttributes = null;
+	
+	/**
+	 * Constructor for creating a component from a XOM Element
+	 *  
+	 * @param element the XOM element representing this 
+	 * @throws InvalidDDMSException if any required information is missing or malformed
+	 */
+	public ProfileList(Element element) throws InvalidDDMSException {
+		try {
+			setXOMElement(element, false);
+			Elements values = element.getChildElements(Profile.getName(getDDMSVersion()), getNamespace());
+			_cachedProfiles = new ArrayList<Profile>();
+			for (int i = 0; i < values.size(); i++) {
+				_cachedProfiles.add(new Profile(values.get(i)));
+			}			
+			_cachedSecurityAttributes = new SecurityAttributes(element);
+			validate();
+		} catch (InvalidDDMSException e) {
+			e.setLocator(getQualifiedName());
+			throw (e);
+		}
+	}
+	
+	/**
+	 * Constructor for creating a component from raw data
+	 *  
+	 * @param profiles the list of profiles (at least 1 required)
+	 * @param securityAttributes security attributes (required)
+	 * @throws InvalidDDMSException if any required information is missing or malformed
+	 */
+	public ProfileList(List<Profile> profiles, SecurityAttributes securityAttributes) throws InvalidDDMSException {
+		try {
+			DDMSVersion version = DDMSVersion.getCurrentVersion();
+			Element element = Util.buildElement(PropertyReader.getProperty("ntk.prefix"), ProfileList.getName(version),
+				version.getNtkNamespace(), null);
+			setXOMElement(element, false);
+			if (profiles == null)
+				profiles = Collections.emptyList();
+			for (Profile profile : profiles) {
+				getXOMElement().appendChild(profile.getXOMElementCopy());
+			}
+			_cachedProfiles = profiles;
+			_cachedSecurityAttributes = (securityAttributes == null ? new SecurityAttributes(null, null, null)
+				: securityAttributes);
+			_cachedSecurityAttributes.addTo(element);
+			validate();
+		} catch (InvalidDDMSException e) {
+			e.setLocator(getQualifiedName());
+			throw (e);
+		}
+	}
+			
+	/**
+	 * Validates the component.
+	 * 
+	 * <table class="info"><tr class="infoHeader"><th>Rules</th></tr><tr><td class="infoBody">
+	 * <li>The qualified name of the element is correct.</li>
+	 * <li>At least 1 profile is required.</li>
+	 * <li>A classification is required.</li>
+	 * <li>At least 1 ownerProducer exists and is non-empty.</li>
+	 * <li>This component cannot exist until DDMS 4.0 or later.</li>
+	 * </td></tr></table>
+	 * 
+	 * @see AbstractBaseComponent#validate()
+	 * @throws InvalidDDMSException if any required information is missing or malformed
+	 */
+	protected void validate() throws InvalidDDMSException {
+		Util.requireQName(getXOMElement(), getNamespace(), ProfileList.getName(getDDMSVersion()));
+		if (getProfiles().isEmpty())
+			throw new InvalidDDMSException("At least one profile is required.");
+		Util.requireDDMSValue("security attributes", getSecurityAttributes());
+		getSecurityAttributes().requireClassification();
+		
+		// Should be reviewed as additional versions of DDMS are supported.
+		requireVersion("4.0");
+		
+		super.validate();
+	}
+		
+	/**
+	 * @see AbstractBaseComponent#getOutput(boolean, String)
+	 */
+	public String getOutput(boolean isHTML, String prefix) {
+		prefix = Util.getNonNullString(prefix) + "profileList.";
+		StringBuffer text = new StringBuffer();
+		for (Profile value : getProfiles())
+			text.append(value.getOutput(isHTML, prefix));
+		text.append(getSecurityAttributes().getOutput(isHTML, prefix));
+		return (text.toString());
+	}
+		
+	/**
+	 * @see AbstractBaseComponent#getNestedComponents()
+	 */
+	protected List<IDDMSComponent> getNestedComponents() {
+		List<IDDMSComponent> list = new ArrayList<IDDMSComponent>();
+		list.addAll(getProfiles());
+		return (list);
+	}
+	
+	/**
+	 * @see Object#equals(Object)
+	 */
+	public boolean equals(Object obj) {
+		if (!super.equals(obj) || !(obj instanceof ProfileList))
+			return (false);
+		return (true);		
+	}
+
+	/**
+	 * Accessor for the element name of this component, based on the version of DDMS used
+	 * 
+	 * @param version the DDMSVersion
+	 * @return an element name
+	 */
+	public static String getName(DDMSVersion version) {
+		Util.requireValue("version", version);
+		return ("AccessProfileList");
+	}
+	
+	/**
+	 * Accessor for the list of profile values (1-many)
+	 */
+	public List<Profile> getProfiles() {
+		return (Collections.unmodifiableList(_cachedProfiles));
+	}	
+	
+	/**
+	 * Accessor for the Security Attributes. Will always be non-null even if the attributes are not set.
+	 */
+	public SecurityAttributes getSecurityAttributes() {
+		return (_cachedSecurityAttributes);
+	}
+	
+	/**
+	 * Builder for this DDMS component.
+	 * 
+	 * @see IBuilder
+	 * @author Brian Uri!
+	 * @since 2.0.0
+	 */
+	public static class Builder implements IBuilder, Serializable {
+		private static final long serialVersionUID = 7851044806424206976L;
+		private List<Profile.Builder> _profiles;
+		private SecurityAttributes.Builder _securityAttributes;
+		
+		/**
+		 * Empty constructor
+		 */
+		public Builder() {}
+		
+		/**
+		 * Constructor which starts from an existing component.
+		 */
+		public Builder(ProfileList profileList) {
+			for (Profile value : profileList.getProfiles())
+				getProfiles().add(new Profile.Builder(value));
+			setSecurityAttributes(new SecurityAttributes.Builder(profileList.getSecurityAttributes()));
+		}
+		
+		/**
+		 * @see IBuilder#commit()
+		 */
+		public ProfileList commit() throws InvalidDDMSException {
+			if (isEmpty())
+				return (null);
+			List<Profile> values = new ArrayList<Profile>();
+			for (IBuilder builder : getProfiles()) {
+				Profile component = (Profile) builder.commit();
+				if (component != null)
+					values.add(component);
+			}
+			return (new ProfileList(values, getSecurityAttributes().commit()));
+		}
+
+		/**
+		 * @see IBuilder#isEmpty()
+		 */
+		public boolean isEmpty() {
+			boolean hasValueInList = false;
+			for (IBuilder builder : getProfiles())
+				hasValueInList = hasValueInList || !builder.isEmpty();
+			return (!hasValueInList && getSecurityAttributes().isEmpty());
+		}
+		
+		/**
+		 * Builder accessor for the values
+		 */
+		public List<Profile.Builder> getProfiles() {
+			if (_profiles == null)
+				_profiles = new LazyList(Profile.Builder.class);
+			return _profiles;
+		}
+		
+		/**
+		 * Builder accessor for the securityAttributes
+		 */
+		public SecurityAttributes.Builder getSecurityAttributes() {
+			if (_securityAttributes == null)
+				_securityAttributes = new SecurityAttributes.Builder();
+			return _securityAttributes;
+		}
+		
+		/**
+		 * Builder accessor for the securityAttributes
+		 */
+		public void setSecurityAttributes(SecurityAttributes.Builder securityAttributes) {
+			_securityAttributes = securityAttributes;
+		}
+	}
+} 
