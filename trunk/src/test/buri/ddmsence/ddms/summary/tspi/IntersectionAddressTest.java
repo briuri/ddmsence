7b97234744dbba842ee958dc91e7a8bbc6e9262b
buri <buri@1f99f4d6-3056-11df-954b-21e382534593>
2013-04-05 00:03:39 +0000

    Issue #193: PostalAddress and TSPI addresses complete.

--- trunk/src/test/buri/ddmsence/ddms/summary/tspi/IntersectionAddressTest.java
+++ trunk/src/test/buri/ddmsence/ddms/summary/tspi/IntersectionAddressTest.java
@@ -44,6 +44,21 @@ public class IntersectionAddressTest extends AbstractBaseTestCase {
 	}
 
 	/**
+	 * Returns a fixture object for testing.
+	 */
+	public static IntersectionAddress getFixture() {
+		try {
+			IntersectionAddress.Builder builder = new IntersectionAddress.Builder();
+			builder.setXml(getExpectedXMLOutput());
+			return (builder.commit());
+		}
+		catch (InvalidDDMSException e) {
+			fail("Could not create fixture: " + e.getMessage());
+		}
+		return (null);
+	}
+	
+	/**
 	 * Attempts to build a component from a XOM element.
 	 * @param element the element to build from
 	 * @param message an expected error message. If empty, the constructor is expected to succeed.
@@ -109,7 +124,7 @@ public class IntersectionAddressTest extends AbstractBaseTestCase {
 	/**
 	 * Returns the expected XML output for this unit test
 	 */
-	private String getExpectedXMLOutput() {
+	private static String getExpectedXMLOutput() {
 		StringBuffer xml = new StringBuffer();
 		xml.append("<tspi:IntersectionAddress ");
 		xml.append("xmlns:tspi=\"http://metadata.ces.mil/mdr/ns/GSIP/tspi/2.0\" ");
--- trunk/src/test/buri/ddmsence/ddms/summary/tspi/USPSPostalDeliveryRouteTest.java
+++ trunk/src/test/buri/ddmsence/ddms/summary/tspi/USPSPostalDeliveryRouteTest.java
@@ -44,6 +44,21 @@ public class USPSPostalDeliveryRouteTest extends AbstractBaseTestCase {
 	}
 
 	/**
+	 * Returns a fixture object for testing.
+	 */
+	public static USPSPostalDeliveryRoute getFixture() {
+		try {
+			USPSPostalDeliveryRoute.Builder builder = new USPSPostalDeliveryRoute.Builder();
+			builder.setXml(getExpectedXMLOutput());
+			return (builder.commit());
+		}
+		catch (InvalidDDMSException e) {
+			fail("Could not create fixture: " + e.getMessage());
+		}
+		return (null);
+	}
+	
+	/**
 	 * Attempts to build a component from a XOM element.
 	 * @param element the element to build from
 	 * @param message an expected error message. If empty, the constructor is expected to succeed.
@@ -109,7 +124,7 @@ public class USPSPostalDeliveryRouteTest extends AbstractBaseTestCase {
 	/**
 	 * Returns the expected XML output for this unit test
 	 */
-	private String getExpectedXMLOutput() {
+	private static String getExpectedXMLOutput() {
 		StringBuffer xml = new StringBuffer();
 		xml.append("<tspi:USPSPostalDeliveryRoute ");
 		xml.append("xmlns:tspi=\"").append(DDMSVersion.getCurrentVersion().getTspiNamespace()).append("\" ");
